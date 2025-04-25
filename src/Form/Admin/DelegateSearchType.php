e364a88de19a30cb79fb0a6ae708d5369b8344e6
Nikola Djukanovic <nikoladjukanovic1989@gmail.com>
2025-04-25 15:24:23 +0200

    fix: sorted namespaces

--- src/Form/Admin/DelegateSearchType.php
+++ src/Form/Admin/DelegateSearchType.php
@@ -2,13 +2,13 @@
 
 namespace App\Form\Admin;
 
+use App\Entity\School;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use App\Entity\School;
 
 class DelegateSearchType extends AbstractType
 {
